1b065f690a1ac2292387e7e7647d0058b4598ea5
Kin <winterheat@gmail.com>
2023-11-23 16:58:33 -0800

    ok

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,35 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
+
+  // await expect(page.locator('body')).toContainText("moddern");
+});
+
+test("has text", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  await expect(page.locator("body")).toContainText("modern");
+});
+
+test("not have text", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  await expect(page.locator("body")).not.toContainText("modern123");
 });
